d4e26186b8eb04b7467d0ecba8fe19d5c604d169
Jônatas Júnior <jonatasjr.019@gmail.com>
2022-10-05 18:00:28 -0300

    testando as janelas do fyne

--- main.go
+++ main.go
@@ -3,13 +3,17 @@ package main
 import (
 	"time"
 
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello World")
+
+	w := a.NewWindow("Clock")
+	w.Resize(fyne.Size{Width: 250, Height: 100})
+	w.SetMaster()
 
 	clock := widget.NewLabel("")
 	updateTime(clock)
@@ -21,7 +25,19 @@ func main() {
 	}()
 
 	w.SetContent(clock)
-	w.ShowAndRun()
+	w.Show()
+
+	w2 := a.NewWindow("TESTE")
+	w2.SetContent(widget.NewLabel("More content"))
+	w2.SetContent(widget.NewButton("Open new", func() {
+		w3 := a.NewWindow("Third")
+		w3.SetContent(widget.NewLabel("Third"))
+		w3.Show()
+	}))
+	w2.Show()
+
+	a.Run()
+	//w.ShowAndRun()
 }
 
 func updateTime(clock *widget.Label) {
