c1428dd1f14e535f846744ce950370acc95d7fa2
Jônatas Júnior <jonatasjr.019@gmail.com>
2022-10-23 18:47:59 -0300

    layout

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -12,16 +15,39 @@ func main() {
 	myApp := app.New()
 	myWindow := myApp.NewWindow("Choice Widgets")
 
-	check := widget.NewCheck("Optional", func(value bool) {
-		log.Println("Check set to", value)
-	})
-	radio := widget.NewRadioGroup([]string{"Option 1", "Option 2"}, func(value string) {
-		log.Println("Radio set to", value)
-	})
 	combo := widget.NewSelect([]string{"Option 1", "Option 2"}, func(value string) {
 		log.Println("Select set to", value)
 	})
 
-	myWindow.SetContent(container.NewVBox(check, radio, combo))
+	separator := widget.NewSeparator()
+
+	openFolder := widget.NewButton("Folder Open", func() {
+		dialog.ShowFolderOpen(func(list fyne.ListableURI, err error) {
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			if list == nil {
+				log.Println("Cancelled")
+				return
+			}
+		}, myWindow)
+	})
+
+	var data = [][]string{[]string{"top left", "top right"},
+		[]string{"bottom left", "bottom right"}}
+
+	list := widget.NewTable(
+		func() (int, int) {
+			return len(data), len(data[0])
+		},
+		func() fyne.CanvasObject {
+			return widget.NewLabel("wide content")
+		},
+		func(i widget.TableCellID, o fyne.CanvasObject) {
+			o.(*widget.Label).SetText(data[i.Row][i.Col])
+		})
+	list.OnSelected(widget.TableCellID{ Row: 0,  Col: 0})
+	myWindow.SetContent(container.NewVBox(combo, openFolder, separator, list))
 	myWindow.ShowAndRun()
 }
